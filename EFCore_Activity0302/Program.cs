77afa9ffab4fe660a85bc5e3f3ec3aaf0e48fa2e
Alexander Rojas Benavides <alexrrojas.b@gmail.com>
2022-07-05 17:55:11 -0600

    add start project for EFCore Code First

---
+++ EFCore_Activity0302/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace EFCore_Activity0302
+{
+    public class Program
+    {
+        private static IConfigurationRoot _configuration;
+        //private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
+        static void Main(string[] args)
+        {
+            BuildOptions();
+        }
+        static void BuildOptions()
+        {
+            _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+            //_optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+            //_optionsBuilder.UseNpgsql(_configuration.GetConnectionString("InventoryManager"));
+        }
+    }
+}
