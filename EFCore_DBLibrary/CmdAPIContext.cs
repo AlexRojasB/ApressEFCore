eef9ff54ddfeb2bb3e72761e7263d44acec8de3e
Alexander Rojas Benavides <alexrrojas.b@gmail.com>
2022-07-04 20:53:46 -0600

    add Entity Framework scaffolding

---
+++ EFCore_DBLibrary/CmdAPIContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EFCore_DBLibrary
+{
+    public partial class CmdAPIContext : DbContext
+    {
+        public CmdAPIContext()
+        {
+        }
+
+        public CmdAPIContext(DbContextOptions<CmdAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Command> Commands { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("User ID=cmddbuser; Password=pa55w0rd!; Host=localhost; Port=5432; Database=CmdAPI; Pooling=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ EFCore_DBLibrary/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_DBLibrary
+{
+    public partial class Command
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(250)]
+        public string HowTo { get; set; } = null!;
+        public string Platform { get; set; } = null!;
+        public string CommandLine { get; set; } = null!;
+    }
+}
