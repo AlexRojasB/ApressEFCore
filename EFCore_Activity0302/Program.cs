0ada18a816cefde36d292a713851ae0e4808fb4c
Alexander Rojas Benavides <alexrrojas.b@gmail.com>
2022-07-06 18:36:25 -0600

    code first setup

--- EFCore_Activity0302/Program.cs
+++ EFCore_Activity0302/Program.cs
@@ -1,20 +1,58 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using EFCore_DBLibrary;
+using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EFCore_Activity0302
 {
     public class Program
     {
         private static IConfigurationRoot _configuration;
-        //private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
+        private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
         static void Main(string[] args)
         {
             BuildOptions();
+            EnsureItems();
+            ListInventory();
         }
         static void BuildOptions()
         {
             _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
-            //_optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
-            //_optionsBuilder.UseNpgsql(_configuration.GetConnectionString("InventoryManager"));
+            _optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+            _optionsBuilder.UseNpgsql(_configuration.GetConnectionString("InventoryConnection"));
+        }
+
+        static void EnsureItems()
+        {
+            EnsureItem("Batman Begins");
+            EnsureItem("Inception");
+            EnsureItem("Remember the Titans");
+            EnsureItem("Star Wars: The Empire Strikes Back");
+            EnsureItem("Top Gun");
+        }
+
+        private static void EnsureItem(string name)
+        {
+            using (var db = new InventoryDbContext(_optionsBuilder.Options))
+            {
+                //determine if item exists:
+                var existingItem = db.Items.FirstOrDefault(x => x.Name.ToLower() == name.ToLower());
+                if (existingItem == null)
+                {
+                    //doesn't exist, add it.
+                    var item = new Item() { Name = name };
+                    db.Items.Add(item);
+                    db.SaveChanges();
+                }
+            }
+        }
+        private static void ListInventory()
+        {
+            using (var db = new InventoryDbContext(_optionsBuilder.Options))
+            {
+                var items = db.Items.OrderBy(x => x.Name).ToList();
+                items.ForEach(x => Console.WriteLine($"New Item: {x.Name}"));
+            }
         }
     }
 }
---
+++ EFCore_DBLibrary/InventoryDbContext.cs
@@ -0,0 +1,28 @@
+﻿using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_DBLibrary
+{
+    public class InventoryDbContext : DbContext
+    {
+        public InventoryDbContext()
+        {
+
+        }
+
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("User ID=cmddbuser; Password=pa55w0rd!; Host=localhost; Port=5432; Database=ApressEFCoreInventory; Pooling=true;");
+            }
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
