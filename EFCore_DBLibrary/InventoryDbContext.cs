5ae3e61057a2f9f4a9638b6d31be8a83bf35f57d
Alexander Rojas Benavides <alexrrojas.b@gmail.com>
2022-07-11 19:53:05 -0600

    change tracking

--- EFCore_DBLibrary/InventoryDbContext.cs
+++ EFCore_DBLibrary/InventoryDbContext.cs
@@ -10,7 +10,7 @@ namespace EFCore_DBLibrary
 
         }
 
-        public InventoryDbContext(DbContextOptions<InventoryDbContext> options): base(options)
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options)
         {
 
         }
@@ -21,6 +21,38 @@ namespace EFCore_DBLibrary
             {
                 optionsBuilder.UseNpgsql("User ID=cmddbuser; Password=pa55w0rd!; Host=localhost; Port=5432; Database=ApressEFCoreInventory; Pooling=true;");
             }
+        }
+        private const string _systemUserId = "2fd28110-93d0-427d-9207-d55dbca680fa";
+
+        public override int SaveChanges()
+        {
+            var tracker = ChangeTracker;
+            foreach (var entry in tracker.Entries())
+            {
+                var referenceEntity = entry.Entity as FullAuditModel;
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        referenceEntity.CreatedDate = DateTime.Now;
+                        if (string.IsNullOrWhiteSpace(referenceEntity.CreatedByUserId))
+                        {
+                            referenceEntity.CreatedByUserId = _systemUserId;
+                        }
+                        break;
+                    case EntityState.Deleted:
+                    case EntityState.Modified:
+                        referenceEntity.LastModifiedDate = DateTime.Now;
+                        if (string.IsNullOrWhiteSpace(referenceEntity.LastModifiedUserId))
+                        {
+                            referenceEntity.LastModifiedUserId = _systemUserId;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return base.SaveChanges();
         }
 
         public DbSet<Item> Items { get; set; }
